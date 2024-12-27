2e8cc4fc1b94630e886a0c214adab1b340d8ae61
miguelcardortega <miguelcardortega@gmail.com>
2024-12-27 19:54:55 -0400

    Add files via upload

--- mokepon/js/mokepon.js
+++ mokepon/js/mokepon.js
@@ -1,26 +1,74 @@
+let ataqueJugador 
+
 function iniciarJuego(){
     let botonMascotaJugador = document.getElementById("boton-mascota")
 botonMascotaJugador.addEventListener("click", seleccionarMascotaJugador)
+
+    let botonFuego = document.getElementById("boton-fuego")
+    botonFuego.addEventListener("click", ataqueFuego)
+
+    let botonAgua = document.getElementById("boton-agua")
+    botonAgua.addEventListener("click", ataqueAgua)
+
+    let botonTierra = document.getElementById("boton-tierra")
+    botonTierra.addEventListener("click", ataqueTierra)
 }
 
 function seleccionarMascotaJugador() {
     let inputHipodoge = document.getElementById("hipodoge")
     let inputCapipepo = document.getElementById("capipepo")
-    let inputratigueya = document.getElementById("ratigueya")
-
+    let inputRatigueya = document.getElementById("ratigueya")
+    let spanmascotaJugador = document.getElementById("mascota-jugador")
+    
     if (inputHipodoge.checked){
-        alert("seleccionaste a Hipodoge")
+        spanmascotaJugador.innerHTML = "Hipodoge"
 
     } else if (inputCapipepo.checked){
-        alert("seleccionaste a Capipepo")
+        spanmascotaJugador.innerHTML = "Capipepo" 
 
-    } else if (inputratigueya.checked){
-        alert("seleccionaste a Ratigueya")
+    } else if (inputRatigueya.checked){
+        spanmascotaJugador.innerHTML = "Ratigueya"
 
     } else {
         alert("selecciona una mascota")
     }
-    
+
+    seleccionarMascotaEnemigo()
+}
+
+function aleatorio(min, max){
+    return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+function seleccionarMascotaEnemigo() {
+
+    eleccionEnemigo = aleatorio(1,3)
+    let spanmascotaEnemigo = document.getElementById
+    ("mascota-enemigo")
+
+    if (eleccionEnemigo == 1) {
+        spanmascotaEnemigo.innerHTML = "Hipodoge"
+
+    } else if (eleccionEnemigo == 2) {
+        spanmascotaEnemigo.innerHTML = "Capipepo"
+
+    } else {
+        spanmascotaEnemigo.innerHTML = "Ratigueya"
+    }
+}
+
+function ataqueFuego() {
+    ataqueJugador = "Fuego🔥"
+}
+
+function ataqueAgua() {
+    ataqueJugador = "Agua💧"
+    alert(ataqueJugador)
+}
+
+function ataqueTierra() {
+    ataqueJugador = "Tierra🌱"
+    alert(ataqueJugador)
 }
 
 window.addEventListener("load", iniciarJuego)
